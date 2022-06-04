3dd7b68c758c99de60175e8bab2d1540f8e37599
jimin <jm131313.kim@gmail.com>
2022-06-04 20:46:43 +0900

    first commit

---
+++ components/AppLayout.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Link from 'next/link';
+import { Menu, Input, Row, Col } from 'antd';
+import 'antd/dist/antd.css';
+import UserProfile from '../components/UserProfile';
+import LoginForm from '../components/LoginForm';
+import styled from 'styled-components';
+
+// 인라인 스타일을 사용하는 경우 리렌더링 되기 때문에 styled component, 또는 useMemo를 사용하여 성능 향상을 시킬 수 있다.
+//const style= useMemo(()=> ({marginTop:10}), []);
+const SearchInput = styled(Inpuy.Search)`
+    verticalalign: 'middle';
+`;
+
+const AppLayout = ({ children }) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const onSearch = () => {};
+    return (
+        <div>
+            <Menu mode="horizontal">
+                <Menu.Item>
+                    <Link href="/">
+                        <a>노드버드</a>
+                    </Link>
+                </Menu.Item>
+                <Menu.Item>
+                    <Link href="/profile">
+                        <a>프로필</a>
+                    </Link>
+                </Menu.Item>
+                <Menu.Item>
+                    <SearchInput
+                        placeholder="input search text"
+                        onSearch={onSearch}
+                        enterButton
+                    />
+                </Menu.Item>
+                <Menu.Item>
+                    <Link href="/signup">
+                        <a>회원가입</a>
+                    </Link>
+                </Menu.Item>
+            </Menu>
+            <Row gutter={8}>
+                <Col xs={24} md={6}>
+                    {isLoggedIn ? <UserProfile /> : <LoginForm />}
+                </Col>
+                <Col xs={24} md={12}>
+                    {children}
+                </Col>
+                <Col xs={24} md={6}>
+                    <a href="" target="_blank" rel="noreferrer noopener">
+                        Made By JiminKim
+                    </a>
+                </Col>
+            </Row>
+        </div>
+    );
+};
+
+AppLayout.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+export default AppLayout;
