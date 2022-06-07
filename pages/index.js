90f0afa75650adf6d2ddaa3f9027a6941fb2e46f
jimin <jm131313.kim@gmail.com>
2022-06-08 00:04:21 +0900

    feat: 더미데이터와 포스트폼 만들기

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
+import React from 'react';
+import AppLayout from '../components/AppLayout';
+import { useSelector } from 'react-redux';
+import PostForm from '../components/PostForm';
+import PostCard from '../components/PostCard';
+
 const Home = () => {
-    return <div>Hello, Next!</div>;
+    const { isLoggedIn } = useSelector((state) => state.user);
+    const { mainPosts } = useSelector((state) => state.post);
+    return (
+        <AppLayout>
+            {isLoggedIn && <PostForm />}
+            {mainPosts.map((post, index) => (
+                <PostCard key={post.id} post={post} />
+            ))}
+        </AppLayout>
+    );
 };
 
 export default Home;
--- reducers/post.js
+++ reducers/post.js
@@ -1,9 +1,61 @@
-const initialState = {
-    mainPosts: [],
+export const initialState = {
+    mainPosts: [
+        {
+            id: 1,
+            User: {
+                id: 1,
+                nickname: '제로초',
+            },
+            content: '첫번째 게시글 ㅎㅎ',
+            Images: [
+                {
+                    src: 'https://avatars.githubusercontent.com/u/3303885?s=48&v=4',
+                },
+                {
+                    src: 'https://avatars.githubusercontent.com/u/3303885?s=48&v=4',
+                },
+                {
+                    src: 'https://avatars.githubusercontent.com/u/3303885?s=48&v=4',
+                },
+            ],
+            Comments: [
+                {
+                    User: {
+                        nickname: 'nero',
+                    },
+                    content: '잼써영',
+                },
+            ],
+        },
+    ],
+    imagePaths: [],
+    postAdded: false,
+};
+
+const ADD_POST = 'ADD_POST';
+export const addPost = {
+    type: ADD_POST,
+};
+
+const dummyPost = {
+    id: 2,
+    content: '더미더미',
+    User: {
+        id: 1,
+        nickname: '제로초',
+    },
+    Images: [],
+    Comments: [],
 };
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
+        case ADD_POST:
+            return {
+                ...state,
+                mainPosts: [dummyPost, ...state.mainPosts],
+                postAdded: true,
+            };
         default:
             return state;
     }
